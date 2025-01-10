5cb5152ef10014bf208d66c7eb8f2838e933f2e5
shivang <shivangshrivastava7078@gmail.com>
2025-01-11 00:52:12 +0530

    Added speech to text service

---
+++ Talk2Ticket/src/main/java/com/Kapture/Talk2Ticket/Talk2TicketApplication.java
@@ -0,0 +1,13 @@
+package com.Kapture.Talk2Ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Talk2TicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Talk2TicketApplication.class, args);
+	}
+
+}
---
+++ Talk2Ticket/src/main/java/com/Kapture/Talk2Ticket/service/SpeechToTextService.java
@@ -0,0 +1,30 @@
+package com.Kapture.Talk2Ticket.service;
+
+import com.google.cloud.speech.v1.RecognitionAudio;
+import com.google.cloud.speech.v1.RecognitionConfig;
+import com.google.cloud.speech.v1.RecognizeResponse;
+import com.google.cloud.speech.v1.SpeechClient;
+import com.google.protobuf.ByteString;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class SpeechToTextService {
+    public String convertSpeechToText(byte[] audioBytes) throws IOException {
+        try (SpeechClient speechClient = SpeechClient.create()) {
+            RecognitionConfig config = RecognitionConfig.newBuilder()
+                    .setEncoding(RecognitionConfig.AudioEncoding.LINEAR16)
+                    .setSampleRateHertz(16000)
+                    .setLanguageCode("en-US")
+                    .build();
+
+            RecognitionAudio audio = RecognitionAudio.newBuilder()
+                    .setContent(ByteString.copyFrom(audioBytes))
+                    .build();
+
+            RecognizeResponse response = speechClient.recognize(config, audio);
+            return response.getResultsList().get(0).getAlternativesList().get(0).getTranscript();
+        }
+    }
+}
